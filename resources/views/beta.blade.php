2ba5e67d94810a1f7662b571b1a423a5f4cae1b7
AndreGartner <andreluizgartner@gmail.com>
2021-01-21 15:07:02 -0300

    initial commit

---
+++ resources/views/beta.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Beta</title>
+    </head>
+    <body>
+        <h1>This is the Beta page</h1>
+        <a href="/alpha">Previous</a>
+    </body>
+</html>
---
+++ tests/Feature/BasicTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Box;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BasicTest extends TestCase
+{
+    public function testBoxContents()
+    {
+        $box = new Box(['toy']);
+        $this->assertTrue($box->has('toy'));
+        $this->assertFalse($box->has('ball'));
+    }
+
+    public function testStartsWithALetter()
+    {
+        $box = new Box(['toy', 'torch', 'ball', 'cat', 'tissue']);
+
+        $results = $box->startsWith('t');
+
+        $this->assertCount(3, $results);
+        $this->assertContains('toy', $results);
+        $this->assertContains('torch', $results);
+        $this->assertContains('tissue', $results);
+
+        // empty array if passed even
+        $this->assertEmpty($box->startsWith('s'));
+    }
+}
